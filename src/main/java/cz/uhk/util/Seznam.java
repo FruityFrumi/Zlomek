de4bc12c4c9d11b0791b34e2507f824c68a7cfd0
zoe <hlavipe1@uhk.cz>
2026-03-02 12:46:56 +0100

    implementace SpojovySeznam a testu

---
+++ src/main/java/cz/uhk/util/Seznam.java
@@ -0,0 +1,13 @@
+package cz.uhk.util;
+
+/**
+ * Rozhrani seznamu prvku
+ * @param <E> typ prvku
+ */
+public interface Seznam<E> {
+    void pridej(E hodnota);
+    void smaz(int pozice);
+    E vrat(int pozice);
+    int pocet();
+
+}
---
+++ src/main/java/cz/uhk/util/impl/SpojovySeznam.java
@@ -0,0 +1,76 @@
+package cz.uhk.util.impl;
+
+import cz.uhk.util.Seznam;
+
+
+public class SpojovySeznam<E> implements Seznam<E> {
+
+    private PrvekSeznamu<E> prvni, posledni;
+
+
+
+
+    @Override
+    public void pridej(E hodnota) {
+        var novy = new PrvekSeznamu<E>(hodnota);
+
+        if (prvni == null) {
+            // seznam je prazni a přidavame první hodnotu
+            prvni = posledni = novy;
+        }
+        else {
+            posledni.dalsi = novy;
+            posledni = novy;
+        }
+    }
+
+    @Override
+    public void smaz(int pozice) {
+        if (pozice == 0) {
+            prvni = prvni.dalsi;
+        }
+        else {
+            var pom = vratPrvek(pozice-1);
+            pom.dalsi = pom.dalsi.dalsi;
+        }
+
+    }
+
+    @Override
+    public E vrat(int pozice) {
+        var prvek = vratPrvek(pozice);
+        return (prvek != null) ? prvek.hodnota :  null;
+    }
+
+    private PrvekSeznamu<E> vratPrvek(int pozice) {
+        var pom = prvni;
+        for (int i = 0; i < pozice && pom != null; i++) {
+            pom = pom.dalsi;
+        }
+        return pom;
+    }
+
+    @Override
+    public int pocet() {
+        var pom = prvni;
+        int pocet = 0;
+        while (pom != null) {
+            pom = pom.dalsi;
+            pocet++;
+        }
+        return pocet;
+    }
+
+
+
+
+}
+
+    class PrvekSeznamu<E> {
+     E hodnota;
+     PrvekSeznamu<E> dalsi;
+
+        public PrvekSeznamu(E hodnota) {
+         this.hodnota = hodnota;
+        }
+    }
